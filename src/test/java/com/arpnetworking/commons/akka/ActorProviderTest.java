0114cd13a85f206b72d01952fd21010ce12883a0
Brandon Arp <brandonarp@gmail.com>
2018-01-04 22:31:26 -0800

    add actor provider (#65)

---
+++ src/test/java/com/arpnetworking/commons/akka/ActorProviderTest.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2018 Inscope Metrics, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.arpnetworking.commons.akka;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.ActorRefFactory;
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * Tests for the ActorProvider class.
+ *
+ * @author Brandon Arp (brandon dot arp at inscopemetrics dot com)
+ */
+public final class ActorProviderTest {
+
+    @Test
+    public void testBuildsInRefFactory() {
+        final String path = "testChild";
+        final ActorRefFactory refFactory = Mockito.mock(ActorRefFactory.class);
+        final ActorProvider provider = new ActorProvider(path, TestActor.class, refFactory);
+        provider.get();
+        Mockito.verify(refFactory).actorOf(Mockito.any(), Mockito.eq(path));
+    }
+
+    @Test
+    public void testInjectorIsInjectedWithBinding() {
+        final String path = "testChild";
+        final ActorRefFactory refFactory = Mockito.mock(ActorRefFactory.class);
+        final ActorProvider actorProvider = new ActorProvider(path, TestActor.class, refFactory);
+        final AbstractModule mod = new TestActorBuilderModule(actorProvider);
+        final Injector injector = Guice.createInjector(mod);
+        injector.getInstance(ActorRef.class);
+        Assert.assertSame(injector, actorProvider.getInjector());
+    }
+
+    private static class TestActor extends AbstractActor {
+
+        @Override
+        public Receive createReceive() {
+            return receiveBuilder().build();
+        }
+    }
+
+    private static class TestActorBuilderModule extends AbstractModule {
+
+        TestActorBuilderModule(final ActorProvider actorProvider) {
+            _actorProvider = actorProvider;
+        }
+
+        @Override
+        protected void configure() {
+            bind(ActorRef.class).toProvider(_actorProvider);
+        }
+
+        private final ActorProvider _actorProvider;
+    }
+}
