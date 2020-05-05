8a926913c11259e4b5a66a333d20b19fcc35bb1f
Ville Koskela <ville@koskelafamily.com>
2020-05-05 16:07:34 -0700

    Improve the RateLimitedLogger to actually be a SLF4J Logger so that it can be used inter-changeably. (#87)

--- src/test/java/com/arpnetworking/commons/slf4j/RateLimitedLoggerTest.java
+++ src/test/java/com/arpnetworking/commons/slf4j/RateLimitedLoggerTest.java
@@ -24,6 +24,7 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.hamcrest.MockitoHamcrest;
 import org.slf4j.Logger;
+import org.slf4j.Marker;
 
 import java.time.Clock;
 import java.time.Duration;
@@ -59,18 +60,18 @@ public final class RateLimitedLoggerTest {
                 Duration.ofSeconds(1),
                 new TickingClock(Clock.systemUTC(), Duration.ofMillis(600)));
 
-        rateLimitedLogger.getLogger().debug("Test1");
+        rateLimitedLogger.getEffectiveLogger().debug("Test1");
         Mockito.verify(_logger).debug("Test1");
         Mockito.verifyNoMoreInteractions(_logger);
 
-        rateLimitedLogger.getLogger().debug("Test2");
+        rateLimitedLogger.getEffectiveLogger().debug("Test2");
         Mockito.verify(_logger, Mockito.never()).debug("Test2");
         Mockito.verifyNoMoreInteractions(_logger);
 
-        rateLimitedLogger.getLogger().debug("Test3");
+        rateLimitedLogger.getEffectiveLogger().debug("Test3");
         Mockito.verify(_logger).debug("Test3");
         Mockito.verify(_logger).info(MockitoHamcrest.argThat(Matchers.matchesPattern(
-                "Skipped 1 messages for 'testRateLimitedLoggerSkippingLogger' since last getLogger at .*")));
+                "Skipped 1 messages for 'testRateLimitedLoggerSkippingLogger' since last log at .*")));
         Mockito.verifyNoMoreInteractions(_logger);
     }
 
@@ -82,21 +83,239 @@ public final class RateLimitedLoggerTest {
                 Duration.ofSeconds(1),
                 new TickingClock(Clock.systemUTC(), Duration.ofMillis(1200)));
 
-        rateLimitedLogger.getLogger().debug("Test1");
+        rateLimitedLogger.getEffectiveLogger().debug("Test1");
         Mockito.verify(_logger).debug("Test1");
         Mockito.verifyNoMoreInteractions(_logger);
 
-        rateLimitedLogger.getLogger().debug("Test2");
+        rateLimitedLogger.getEffectiveLogger().debug("Test2");
         Mockito.verify(_logger).debug("Test2");
         Mockito.verifyNoMoreInteractions(_logger);
 
-        rateLimitedLogger.getLogger().debug("Test3");
+        rateLimitedLogger.getEffectiveLogger().debug("Test3");
         Mockito.verify(_logger).debug("Test3");
         Mockito.verifyNoMoreInteractions(_logger);
     }
 
+    @Test
+    public void testSlf4jPassThroughGetName() {
+        final RateLimitedLogger rateLimitedLogger = new RateLimitedLogger(
+                "testSlf4jPassThrough",
+                _logger,
+                Duration.ofSeconds(1),
+                Clock.systemDefaultZone(),
+                () -> true);
+
+        // Trace
+        rateLimitedLogger.getName();
+        Mockito.verify(_logger).getName();
+    }
+
+    @Test
+    public void testSlf4jPassThroughTrace() {
+        final Throwable throwable = new NullPointerException("test exception");
+        final RateLimitedLogger rateLimitedLogger = new RateLimitedLogger(
+                "testSlf4jPassThrough",
+                _logger,
+                Duration.ofSeconds(1),
+                Clock.systemDefaultZone(),
+                () -> true);
+
+        // Trace
+        rateLimitedLogger.trace("1");
+        Mockito.verify(_logger).trace("1");
+        rateLimitedLogger.trace("2", "3");
+        Mockito.verify(_logger).trace("2", "3");
+        rateLimitedLogger.trace("4", "5", "6", "7");
+        Mockito.verify(_logger).trace("4", "5", "6", "7");
+        rateLimitedLogger.trace("8", "9", "10");
+        Mockito.verify(_logger).trace("8", "9", "10");
+        rateLimitedLogger.trace("11", throwable);
+        Mockito.verify(_logger).trace("11", throwable);
+        rateLimitedLogger.isTraceEnabled();
+        Mockito.verify(_logger).isTraceEnabled();
+
+        rateLimitedLogger.trace(_marker, "12");
+        Mockito.verify(_logger).trace(_marker, "12");
+        rateLimitedLogger.trace(_marker, "13", "14");
+        Mockito.verify(_logger).trace(_marker, "13", "14");
+        rateLimitedLogger.trace(_marker, "15", "16", "17", "18");
+        Mockito.verify(_logger).trace(_marker, "15", "16", "17", "18");
+        rateLimitedLogger.trace(_marker, "19", "20", "21");
+        Mockito.verify(_logger).trace(_marker, "19", "20", "21");
+        rateLimitedLogger.trace(_marker, "22", throwable);
+        Mockito.verify(_logger).trace(_marker, "22", throwable);
+        rateLimitedLogger.isTraceEnabled(_marker);
+        Mockito.verify(_logger).isTraceEnabled(_marker);
+    }
+
+    @Test
+    public void testSlf4jPassThroughDebug() {
+        final Throwable throwable = new NullPointerException("test exception");
+        final RateLimitedLogger rateLimitedLogger = new RateLimitedLogger(
+                "testSlf4jPassThrough",
+                _logger,
+                Duration.ofSeconds(1),
+                Clock.systemDefaultZone(),
+                () -> true);
+
+        // Debug
+        rateLimitedLogger.debug("1");
+        Mockito.verify(_logger).debug("1");
+        rateLimitedLogger.debug("2", "3");
+        Mockito.verify(_logger).debug("2", "3");
+        rateLimitedLogger.debug("4", "5", "6", "7");
+        Mockito.verify(_logger).debug("4", "5", "6", "7");
+        rateLimitedLogger.debug("8", "9", "10");
+        Mockito.verify(_logger).debug("8", "9", "10");
+        rateLimitedLogger.debug("11", throwable);
+        Mockito.verify(_logger).debug("11", throwable);
+        rateLimitedLogger.isDebugEnabled();
+        Mockito.verify(_logger).isDebugEnabled();
+
+        rateLimitedLogger.debug(_marker, "12");
+        Mockito.verify(_logger).debug(_marker, "12");
+        rateLimitedLogger.debug(_marker, "13", "14");
+        Mockito.verify(_logger).debug(_marker, "13", "14");
+        rateLimitedLogger.debug(_marker, "15", "16", "17", "18");
+        Mockito.verify(_logger).debug(_marker, "15", "16", "17", "18");
+        rateLimitedLogger.debug(_marker, "19", "20", "21");
+        Mockito.verify(_logger).debug(_marker, "19", "20", "21");
+        rateLimitedLogger.debug(_marker, "22", throwable);
+        Mockito.verify(_logger).debug(_marker, "22", throwable);
+        rateLimitedLogger.isDebugEnabled(_marker);
+        Mockito.verify(_logger).isDebugEnabled(_marker);
+    }
+
+    @Test
+    public void testSlf4jPassThroughInfo() {
+        final Throwable throwable = new NullPointerException("test exception");
+        final RateLimitedLogger rateLimitedLogger = new RateLimitedLogger(
+                "testSlf4jPassThrough",
+                _logger,
+                Duration.ofSeconds(1),
+                Clock.systemDefaultZone(),
+                () -> true);
+
+        // Info
+        rateLimitedLogger.info("1");
+        Mockito.verify(_logger).info("1");
+        rateLimitedLogger.info("2", "3");
+        Mockito.verify(_logger).info("2", "3");
+        rateLimitedLogger.info("4", "5", "6", "7");
+        Mockito.verify(_logger).info("4", "5", "6", "7");
+        rateLimitedLogger.info("8", "9", "10");
+        Mockito.verify(_logger).info("8", "9", "10");
+        rateLimitedLogger.info("11", throwable);
+        Mockito.verify(_logger).info("11", throwable);
+        rateLimitedLogger.isInfoEnabled();
+        Mockito.verify(_logger).isInfoEnabled();
+
+        rateLimitedLogger.info(_marker, "12");
+        Mockito.verify(_logger).info(_marker, "12");
+        rateLimitedLogger.info(_marker, "13", "14");
+        Mockito.verify(_logger).info(_marker, "13", "14");
+        rateLimitedLogger.info(_marker, "15", "16", "17", "18");
+        Mockito.verify(_logger).info(_marker, "15", "16", "17", "18");
+        rateLimitedLogger.info(_marker, "19", "20", "21");
+        Mockito.verify(_logger).info(_marker, "19", "20", "21");
+        rateLimitedLogger.info(_marker, "22", throwable);
+        Mockito.verify(_logger).info(_marker, "22", throwable);
+        rateLimitedLogger.isInfoEnabled(_marker);
+        Mockito.verify(_logger).isInfoEnabled(_marker);
+    }
+
+    @Test
+    public void testSlf4jPassThroughWarn() {
+        final Throwable throwable = new NullPointerException("test exception");
+        final RateLimitedLogger rateLimitedLogger = new RateLimitedLogger(
+                "testSlf4jPassThrough",
+                _logger,
+                Duration.ofSeconds(1),
+                Clock.systemDefaultZone(),
+                () -> true);
+
+        // Warn
+        rateLimitedLogger.warn("1");
+        Mockito.verify(_logger).warn("1");
+        rateLimitedLogger.warn("2", "3");
+        Mockito.verify(_logger).warn("2", "3");
+        rateLimitedLogger.warn("4", "5", "6", "7");
+        Mockito.verify(_logger).warn("4", "5", "6", "7");
+        rateLimitedLogger.warn("8", "9", "10");
+        Mockito.verify(_logger).warn("8", "9", "10");
+        rateLimitedLogger.warn("11", throwable);
+        Mockito.verify(_logger).warn("11", throwable);
+        rateLimitedLogger.isWarnEnabled();
+        Mockito.verify(_logger).isWarnEnabled();
+
+        rateLimitedLogger.warn(_marker, "12");
+        Mockito.verify(_logger).warn(_marker, "12");
+        rateLimitedLogger.warn(_marker, "13", "14");
+        Mockito.verify(_logger).warn(_marker, "13", "14");
+        rateLimitedLogger.warn(_marker, "15", "16", "17", "18");
+        Mockito.verify(_logger).warn(_marker, "15", "16", "17", "18");
+        rateLimitedLogger.warn(_marker, "19", "20", "21");
+        Mockito.verify(_logger).warn(_marker, "19", "20", "21");
+        rateLimitedLogger.warn(_marker, "22", throwable);
+        Mockito.verify(_logger).warn(_marker, "22", throwable);
+        rateLimitedLogger.isWarnEnabled(_marker);
+        Mockito.verify(_logger).isWarnEnabled(_marker);
+    }
+
+    @Test
+    public void testSlf4jPassThroughError() {
+        final Throwable throwable = new NullPointerException("test exception");
+        final RateLimitedLogger rateLimitedLogger = new RateLimitedLogger(
+                "testSlf4jPassThrough",
+                _logger,
+                Duration.ofSeconds(1),
+                Clock.systemDefaultZone(),
+                () -> true);
+
+        // Error
+        rateLimitedLogger.error("1");
+        Mockito.verify(_logger).error("1");
+        rateLimitedLogger.error("2", "3");
+        Mockito.verify(_logger).error("2", "3");
+        rateLimitedLogger.error("4", "5", "6", "7");
+        Mockito.verify(_logger).error("4", "5", "6", "7");
+        rateLimitedLogger.error("8", "9", "10");
+        Mockito.verify(_logger).error("8", "9", "10");
+        rateLimitedLogger.error("11", throwable);
+        Mockito.verify(_logger).error("11", throwable);
+        rateLimitedLogger.isErrorEnabled();
+        Mockito.verify(_logger).isErrorEnabled();
+
+        rateLimitedLogger.error(_marker, "12");
+        Mockito.verify(_logger).error(_marker, "12");
+        rateLimitedLogger.error(_marker, "13", "14");
+        Mockito.verify(_logger).error(_marker, "13", "14");
+        rateLimitedLogger.error(_marker, "15", "16", "17", "18");
+        Mockito.verify(_logger).error(_marker, "15", "16", "17", "18");
+        rateLimitedLogger.error(_marker, "19", "20", "21");
+        Mockito.verify(_logger).error(_marker, "19", "20", "21");
+        rateLimitedLogger.error(_marker, "22", throwable);
+        Mockito.verify(_logger).error(_marker, "22", throwable);
+        rateLimitedLogger.isErrorEnabled(_marker);
+        Mockito.verify(_logger).isErrorEnabled(_marker);
+    }
+
+    @Test
+    @SuppressWarnings("deprecation")
+    public void testGetLogger() {
+        final RateLimitedLogger rateLimitedLogger = new RateLimitedLogger(
+                "testRateLimitedLoggerNoSkippingLogger",
+                _logger,
+                Duration.ofSeconds(1),
+                Clock.systemDefaultZone(),
+                () -> true);
+        Assert.assertSame(_logger, rateLimitedLogger.getLogger());
+    }
+
     @Mock
     private Logger _logger;
+    @Mock
+    private Marker _marker;
 
     private static final class TickingClock extends Clock {
 
