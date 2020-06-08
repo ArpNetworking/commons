bc5df2ebaf30e1433f407b20a581614dbf310401
Spencer Pearson <spencerpearson@dropbox.com>
2020-06-08 14:37:50 -0700

    improve ThreadLocalBuildableTestHelper failure message

--- src/main/java/com/arpnetworking/commons/test/ThreadLocalBuildableTestHelper.java
+++ src/main/java/com/arpnetworking/commons/test/ThreadLocalBuildableTestHelper.java
@@ -69,7 +69,10 @@ public final class ThreadLocalBuildableTestHelper {
         for (final Method method : BuilderTestUtility.getSetters(builderClass)) {
             final Optional<Field> field = BuilderTestUtility.getField(builderClass, method);
             if (!field.isPresent()) {
-                throw new IllegalStateException("Builder setter does not have matching field");
+                throw new IllegalStateException(String.format(
+                        "Builder setter %s does not have matching field",
+                        method.getName()
+                ));
             }
             final Object expectedValue = BuilderTestUtility.getFieldValue(builder, field.get());
             Assert.assertNotNull("Expected value for " + field.get().getName() + " can not be null", expectedValue);
